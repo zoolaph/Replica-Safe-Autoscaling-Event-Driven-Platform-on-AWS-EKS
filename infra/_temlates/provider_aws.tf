40f467d3d3ae8249e10c76b7c9d7a3782012418e
zoolaph <farouq.kasah@gmail.com>
2025-12-30 20:27:14 +0100

    docs: add cost safety rails + destroy script + tagging template + cost-audit

---
+++ infra/_temlates/provider_aws.tf
@@ -0,0 +1,22 @@
+terraform {
+    required_version = "~> 1.7.5"
+    required_providers {
+        aws = {
+            source = "hashicorp/aws
+            version = "~> 5.0"
+        }
+    }
+}
+
+provider "aws" {
+    region = "eu-west-3"
+
+    default_tags {
+        tags = {
+            Project ="ReplicaSafeEKS"
+            Env = "dev"
+            Owner = "admin-farouq"
+            ManagedBy = "Terraform"
+        }
+    }
+}
