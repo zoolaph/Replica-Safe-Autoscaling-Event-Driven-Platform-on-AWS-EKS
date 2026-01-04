c44a15ee946c922a506a79af09a3252ef0de4c78
zoolaph <farouq.kasah@gmail.com>
2026-01-04 22:16:03 +0100

    feat : correction of hashicorp/aws version

--- infra/_templates/provider_aws.tf
+++ infra/_templates/provider_aws.tf
@@ -1,22 +1,22 @@
 terraform {
-    required_version = ">= 1.7.5, < 2.0.0"
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = ">= 6.23.0, < 7.0.0"
-        }
+  required_version = ">= 1.7.5, < 2.0.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.23.0, < 7.0.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "eu-west-3"
+  region = "eu-west-3"
 
-    default_tags {
-        tags = {
-            Project ="ReplicaSafeEKS"
-            Env = "dev"
-            Owner = "admin-farouq"
-            ManagedBy = "Terraform"
-        }
+  default_tags {
+    tags = {
+      Project   = "ReplicaSafeEKS"
+      Env       = "dev"
+      Owner     = "admin-farouq"
+      ManagedBy = "Terraform"
     }
+  }
 }
