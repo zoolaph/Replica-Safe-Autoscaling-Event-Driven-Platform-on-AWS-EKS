8d1d78c02a5e48450190f0455488e8966d30a51a
zoolaph <94008067+zoolaph@users.noreply.github.com>
2026-01-07 20:45:31 +0100

    Feat/ebs csi metrics (#20)
    * simple fixes
    
    * feat(platform): add EBS CSI + metrics-server baseline (PVC + kubectl top)

---
+++ infra/environments/dev/addon_ebs_csi.tf
@@ -0,0 +1,23 @@
+module "irsa_ebs_csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "~> 5.0"
+
+  role_name             = "${module.eks.cluster_name}-ebs-csi"
+  attach_ebs_csi_policy = true
+
+  oidc_providers = {
+    main = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["kube-system:ebs-csi-controller-sa"]
+    }
+  }
+}
+resource "aws_eks_addon" "ebs_csi" {
+  cluster_name = module.eks.cluster_name
+  addon_name   = "aws-ebs-csi-driver"
+
+  service_account_role_arn = module.irsa_ebs_csi.iam_role_arn
+
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+}
