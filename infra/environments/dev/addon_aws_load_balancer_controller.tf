903dc04a5748d3402cedf284a450d8c559b627dd
zoolaph <farouq.kasah@gmail.com>
2026-01-08 21:49:29 +0100

    feat: IAM policy + IRSA role

---
+++ infra/environments/dev/addon_aws_load_balancer_controller.tf
@@ -0,0 +1,28 @@
+# AWS Load Balancer Controller - IAM policy + IRSA role 
+###########################################################
+
+resource "aws_iam_policy" "aws_load_balancer_controller" {
+    name       = "${module.eks.cluster_name}-aws-load-balancer-controller-policy"
+    policy     = file("${path.module}/../../_templates/iam/aws-load-balancer-controller-policy.json")
+}
+
+module "irsa_aws_load_balancer_controller" {
+    source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+    version = "~> 5.0"
+    role_name = "${module.eks.cluster_name}-aws-load-balancer-controller-role"
+    role_policy_arns = {
+        lbc = aws_iam_policy.aws_load_balancer_controller.arn
+    }
+
+    oidc_providers = {
+        main = {
+            provider_arn               = module.eks.oidc_provider_arn
+            namespace_service_accounts = ["kube-system:aws-load-balancer-controller"]
+        }
+    } 
+}
+
+output "aws_load_balancer_controller_iam_role_arn" {
+    value = module.irsa_aws_load_balancer_controller.iam_role_arn
+}
+
